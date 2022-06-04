ab063d01abfa6edfbc62d0bad932bc06ee833b25
kiplangat01 <97243798+kiplangat01@users.noreply.github.com>
2022-06-04 16:26:07 +0300

    ft:made model class for image post

--- post/models.py
+++ post/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=100),
+    content = models.ImageField()
+    dateposted = models.DateTimeField(default=timezone)
