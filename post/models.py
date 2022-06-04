ebe9d4b313b0196b6d9e57693114d524648c0f7c
kiplangat01 <97243798+kiplangat01@users.noreply.github.com>
2022-06-04 16:44:10 +0300

    ft:made user tabel in models.py

--- post/models.py
+++ post/models.py
@@ -1,7 +1,9 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 
 class Post(models.Model):
-    title = models.CharField(max_length=100),
+    title = models.CharField(max_length=100)
     content = models.ImageField()
-    dateposted = models.DateTimeField(default=timezone)
+    dateposted = models.DateTimeField(default=timezone.now)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
