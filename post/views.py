1f3ffb689a12fcdb98b8e17cd0b16b1507989c4d
kiplangat01 <97243798+kiplangat01@users.noreply.github.com>
2022-06-05 21:18:30 +0300

    ft:creted new users

--- post/views.py
+++ post/views.py
@@ -2,9 +2,11 @@ from django.shortcuts import render
 from .models import Post
 
 
+
+
 def home(request):
     context = {
-        'posts' : Post.objects.all()
+        'post' : Post.objects.all()
     }
      
     return render(request, 'post/home.html')
