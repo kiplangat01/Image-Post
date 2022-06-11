4325c38b23af50b8a5c556cec5301703093b9343
kiplangat01 <97243798+kiplangat01@users.noreply.github.com>
2022-06-11 23:23:36 +0300

    ft:aded password reset functionality

--- imagepost/settings.py
+++ imagepost/settings.py
@@ -162,5 +162,11 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend' 
+EMAIL_HOST='smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'meshnobie@gmail.com'
+EMAIL_HOST_PASSWORD = 'kzgovjuizlyxwvil'
 # Configure Django App for Heroku.
 django_heroku.settings(locals())
