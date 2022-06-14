2278a7741e2c8d585ee1bfd488a4ea330e315d01
kiplangat01 <97243798+kiplangat01@users.noreply.github.com>
2022-06-15 01:09:23 +0300

    ft:added cloudinary fields for users profies

--- users/models.py
+++ users/models.py
@@ -1,26 +1,19 @@
 from django.db import models
 from django.contrib.auth.models import User
-from cloudinary.models import CloudinaryField
 from PIL import Image
 
 
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    image = CloudinaryField('image', default='default.jpg')
-    # image = models.ImageField(default='default.jpg', upload_to='profile_pics')
+    image = models.ImageField(default='default.jpg', upload_to='profile_pics')
 
 
     def __str__(self):
         return f'{self.user.username} Profile'
 
-    def save_image(self):
-        
-        self.save()
-
-    def delete_image(self):
-       
-        self.delete()
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
 
         img = Image.open(self.image.path)
 
